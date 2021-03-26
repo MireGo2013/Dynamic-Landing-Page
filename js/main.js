2176fedb6a70588b2ab738d39c43cb110ac1b89e
MireGo2013 <sobol.v92@gmail.com>
2021-03-26 12:04:52 +0300

    style: corrected code

--- js/main.js
+++ js/main.js
@@ -1,113 +1,109 @@
 //DOM Elements
 const time = document.getElementById('time'),
-	greeting = document.getElementById('greeting'),
-	name = document.getElementById('name'),
-	focus = document.getElementById('focus');
+	greeting = document.getElementById('greeting');
+user = document.getElementById('name');
+focus = document.getElementById('focus');
+
 
 // Option 
 const showAmPm = true;
 
 // Show Time
 function showTime() {
+	// Output Time
+	time.innerHTML = getTime();
+	setTimeout(showTime, 1000);
+}
+
+//Add Zeros for displayTime
+function addZero(n) {
+	return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+function getTime() {
 	let today = new Date(),
 		hour = today.getHours(),
 		min = today.getMinutes(),
 		sec = today.getSeconds();
-
-
-	// Set AM or PM 
 	const amPm = hour >= 12 ? 'PM' : 'AM';
-
 	// 12hr Format
 	hour = hour % 12 || 12;
-
-	// Output Time
-	time.innerHTML = `${addZero(hour)}<spna>:</spna>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
-
-	setTimeout(showTime, 1000);
+	return `${addZero(hour)}<spna>:</spna>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`
 }
 
-//Add Zeros
-function addZero(n) {
-	return (parseInt(n, 10) < 10 ? '0' : '') + n;
+const LABEL_TEXT = {
+	GOOD_MORNING: 'Good Morning',
+	GOOD_AFTERNOON: 'Good Afternoon',
+	GOOD_NIGHT: 'Good Night',
+}
+
+const IMAGES_BG = {
+	GOOD_MORNING: "url('./img/morning.jpg')",
+	GOOD_AFTERNOON: "url('./img/day.jpg')",
+	GOOD_NIGHT: "url('./img/night.jpg')",
 }
 
 // Set Backgrounds and Greeding
 function setBgGreet() {
 	let today = new Date(),
 		hour = today.getHours();
-
 	if (hour < 12) {
 		//Morning
-		document.body.style.backgroundImage = "url('./img/morning.jpg')";
-		greeting.textContent = 'Good Morning';
+		document.body.style.backgroundImage = IMAGES_BG.GOOD_MORNING;
+		greeting.textContent = LABEL_TEXT.GOOD_MORNING;
 	} else if (hour < 18) {
 		//Afternoon
-		document.body.style.backgroundImage = "url('./img/day.jpg')";
-		greeting.textContent = 'Good Afternoon';
+		document.body.style.backgroundImage = IMAGES_BG.GOOD_AFTERNOON;
+		greeting.textContent = LABEL_TEXT.GOOD_AFTERNOON;
 	} else {
 		// Evening
-		document.body.style.backgroundImage = "url('./img/night.jpg')";
-		greeting.textContent = 'Good Night';
+		document.body.style.backgroundImage = IMAGES_BG.GOOD_NIGHT;
+		greeting.textContent = LABEL_TEXT.GOOD_NIGHT;
 		document.body.style.color = 'white';
 	}
 }
 
-// Get Name
-function getName() {
-	if (localStorage.getItem('name') === null) {
-		name.textContent = '[Enter Your Name]';
+const DEFAULT_TEXT = {
+	name: '[Enter Your Name]',
+	focus: '[Enter Focus]',
+}
+
+function getItemField(field) {
+	if (!field) return; // 
+	const fieldElement = document.getElementById(field)
+	if (!fieldElement) return;
+	if (localStorage.getItem(field) === null) {
+		fieldElement.textContent = DEFAULT_TEXT[field];
 	} else {
-		name.textContent = localStorage.getItem('name');
+		fieldElement.textContent = localStorage.getItem(field);
 	}
 }
 
-// Set Name
-function setName(e) {
+function setItemField(e) {
 	if (e.type === 'keypress') {
 		//Make sure enter is pressed
 		if (e.which == 13 || e.keyCode == 13) {
-			localStorage.setItem('name', e.target.innerText);
-			name.blur();
+			localStorage.setItem(e.target.id, e.target.innerText);
+			e.target.blur();
 			if (e.target.textContent === '') {
-				e.target.textContent = '[Enter Your Name]';
+				e.target.textContent = DEFAULT_TEXT[e.target.id];
 			}
 		}
 	} else {
-		localStorage.setItem('name', e.target.innerText);
+		localStorage.setItem(e.target.id, e.target.innerText);
 	}
 }
 
-// Get Focus
-function getFocus() {
-	if (localStorage.getItem('focus') === null) {
-		focus.textContent = '[Enter Focus]';
-	} else {
-		focus.textContent = localStorage.getItem('focus');
-	}
-}
-
-// Set Focus
-function setFocus(e) {
-	if (e.type === 'keypress') {
-		//Make sure enter is pressed
-		if (e.which == 13 || e.keyCode == 13) {
-			localStorage.setItem('focus', e.target.innerText);
-			focus.blur();
-		}
-	} else {
-		localStorage.setItem('focus', e.target.innerText);
-	}
-}
-
-
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
+user.addEventListener('change', setItemField);
+user.addEventListener('blur', setItemField);
+focus.addEventListener('change', setItemField);
+focus.addEventListener('blur', setItemField);
 
 //Run
 showTime();
 setBgGreet();
-getName();
-getFocus();
+getItemField('focus');
+getItemField('name');
+
+
+
